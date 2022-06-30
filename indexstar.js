4a55fc7be11e548896c4ddf8eec4d048139be9c2
dhruvlk <dhruvlallukarshanwala6165@gmail.com>
2022-06-30 15:57:52 +0530

    pyramid

--- indexstar.js
+++ indexstar.js
@@ -90,3 +90,58 @@ for (let i = 0;i < n;i++) {
     string += "\n"
 }
 console.log(string)
+
+
+
+
+// dimond pyramid
+n = 5
+string = ""
+for (i = 1;i <= n;i++) {
+    for (j = n;j > i;j--) {
+        string += " "
+    }
+    for (k = 0;k < i * 2 - 1;k++) {
+        string += "*"
+    }
+    string += "\n"
+}
+for (i = 1;i <= n - 1;i++) {
+
+    for (j = 0;j < i;j++) {
+        string += " "
+    }
+    for (k = (n - i) * 2 - 1;k > 0;k--) {
+        string += "*"
+    }
+    string += "\n"
+}
+console.log(string)
+
+
+
+// dimond pyramid
+n = 5
+string = ""
+for (i = 1;i <= n * 2;i++) {
+
+    if (i < n) {
+        for (j = n;j > i;j--) {
+            string += " "
+        }
+        for (k = 0;k < i * 2 - 1;k++) {
+            string += "*"
+        }
+    } else {
+        e = i - n
+        for (j = 0;j < e;j++) {
+            string += " "
+        }
+        for (k = (n - e) * 2 - 1;k > 0;k--) {
+            string += "*"
+        }
+    }
+    string += "\n"
+}
+
+console.log(string)
