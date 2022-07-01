84e065a2c880c9398aeab2f15f36e43eb7b2121f
dhruvlk <dhruvlallukarshanwala6165@gmail.com>
2022-07-01 18:32:39 +0530

    Array methods

--- indexstar.js
+++ indexstar.js
@@ -145,3 +145,20 @@ for (i = 1;i <= n * 2;i++) {
 }
 
 console.log(string)
+
+
+
+
+function diamond(val) {
+    let y,
+        w,
+        shape = ''
+
+    for (y = 0;y < val * 2 - 1;y++) {
+        w = y < val ? y : val * 2 - y - 2
+        shape += Array(val - w).join(' ') + Array(w + 1).join('* ') + '*\n'
+    }
+    console.log(shape)
+}
+
+diamond(4)
